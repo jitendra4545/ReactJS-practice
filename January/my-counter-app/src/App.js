a6119aebe424ec49a45ae0e2ec5d17371a6bd7fc
jitendra4545 <jitendra45.js45@gmail.com>
2023-01-09 18:55:18 +0530

    added

---
+++ January/my-counter-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter'
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter Application</h1>
+
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ January/my-counter-app/src/components/Counter.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+    const [count,setCount]=useState(0)
+
+    const handleReduce=()=>{
+         setCount(prev=>prev-1)
+    }
+
+    const handleAdd=()=>{
+     setCount(prev=>prev+1)
+    }
+    
+    
+
+  return (
+    <div>
+       <h1>Count value is : {count}</h1>
+       <button disabled={count<=0} onClick={handleReduce}>-</button>
+       <button  onClick={handleAdd}>+</button>
+    </div>
+  )
+}
