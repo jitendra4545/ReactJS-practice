03124711afbf5fe1fc53fc33eb1ffbb58dcd5faa
jitendra4545 <jitendra45.js45@gmail.com>
2023-03-08 18:34:43 +0530

    added

--- MarchRevisionPractice/Dom-1/assinment5/TodoDOM_552626/index.js
+++ MarchRevisionPractice/Dom-1/assinment5/TodoDOM_552626/index.js
@@ -1,34 +1,34 @@
 
 
 
-const myfunction=(e)=>{
-    e.preventDefault()
+const myfunction = (e) => {
+   e.preventDefault()
 
-    let obj={
-    task:document.getElementById('task').value ,
-       priority:document.getElementById('priority').value 
-    }
- display(obj)
+   let obj = {
+      task: document.getElementById('task').value,
+      priority: document.getElementById('priority').value
+   }
+   display(obj)
 
-    }
+}
 
-let form =document.querySelector('form').addEventListener("submit",myfunction)
+let form = document.querySelector('form').addEventListener("submit", myfunction)
 
-let tbody=document.querySelector('tbody')
+let tbody = document.querySelector('tbody')
 
-let display=(data)=>{
-      let tr=document.createElement('tr')
-      
-      let task=document.createElement('td')
-      task.innerText=data.task
-      
-      let priority=document.createElement('td')
-      priority.innerText=data.priority
-      
-      let button=document.createElement('td')
-       button.innerText='Favorite'
-      tr.append(task,priority,button)
-      tbody.append(tr)
+let display = (data) => {
+   let tr = document.createElement('tr')
+
+   let task = document.createElement('td')
+   task.innerText = data.task
+
+   let priority = document.createElement('td')
+   priority.innerText = data.priority
+
+   let button = document.createElement('td')
+   button.innerText = 'Favorite'
+   tr.append(task, priority, button)
+   tbody.append(tr)
 
 }
 
