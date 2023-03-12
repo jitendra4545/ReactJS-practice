1bb136ce6a8c3484e0067fff1d6b8da6a6e2d280
jitendra4545 <jitendra45.js45@gmail.com>
2023-03-12 22:31:25 +0530

    added

---
+++ MarchRevisionPractice/unit-3-practice/js211-c1-template_769205 (1)/scripts/index.js
@@ -0,0 +1,36 @@
+/*
+Use local storage as your database. And store data with key as "mobile_data". The mobile object will be :-
+{
+brand: String,
+name: String,
+price: Number,
+image: String
+}
+
+Example:- 
+{
+brand: "apple", (oneplus/nokia)
+name: "Iphone 13",
+price: 74000,
+image: "https://www.iphone.com/image.jpg"
+}
+
+*/
+
+let mobileData=JSON.parse(localStorage.getItem("mobile_data"))||[]
+
+
+const handleForm=(e)=>{
+e.preventDefault()
+let obj={
+    brand:document.querySelector("#mobile_brand").value ,
+    name:document.querySelector("#mobile_name").value,
+    price:+document.querySelector("#mobile_price").value,
+    image:document.querySelector("#mobile_image").value
+}
+mobileData.push(obj)
+localStorage.setItem("mobile_data",JSON.stringify(mobileData))
+}
+
+
+document.querySelector("form").addEventListener("submit",handleForm)
