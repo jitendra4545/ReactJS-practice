0c23451e482aff58a621b8a70bafbd8e12aaf053
jitendra4545 <jitendra45.js45@gmail.com>
2023-06-23 21:53:30 +0530

    added

---
+++ Assesments/tax-invoice/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Sidebar } from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1> */}
+           <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
